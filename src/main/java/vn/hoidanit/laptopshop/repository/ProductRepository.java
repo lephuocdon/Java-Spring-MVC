e5bc089774bf06bd7daaa16d1cea105bb77a824a
lephuocdon <lephuocdon2002@gmail.com>
2025-02-24 18:03:47 +0700

    CRUD product

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {

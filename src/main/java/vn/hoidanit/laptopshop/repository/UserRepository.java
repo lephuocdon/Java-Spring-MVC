82391db3cd1c90a06071cc9bbc61bc52c9c4bcfc
lephuocdon <lephuocdon2002@gmail.com>
2025-02-26 17:11:27 +0700

    register

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -17,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByEmail(String email);
 
     User findById(long id);
+
+    boolean existsByEmail(String email);
 }

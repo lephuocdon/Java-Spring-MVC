7ccd4fb225975a3dbaa4fccc44606fb2064daef3
lephuocdon <lephuocdon2002@gmail.com>
2025-02-26 14:17:28 +0700

    custom validator

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -14,6 +14,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import vn.hoidanit.laptopshop.service.validator.StrongPassword;
 
 @Entity
 @Table(name = "users")
@@ -28,6 +29,7 @@ public class User {
 
     @NotNull
     @Size(min = 2, message = "Password must be at least 2 characters")
+    @StrongPassword(message = "Password phải có 8 ký tự")
     private String password;
 
     @NotNull

da6b4bc559b4ead377570bd155c70d26006717fb
lephuocdon <lephuocdon2002@gmail.com>
2025-02-26 21:45:33 +0700

    loadUserByUsername

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -60,4 +60,7 @@ public class UserService {
         return this.userRepository.existsByEmail(email);
     }
 
+    public User getUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
 }

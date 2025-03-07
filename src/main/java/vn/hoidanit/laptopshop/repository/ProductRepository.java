283fae7e4c4569f811205579febdb0c9b3f813b9
lephuocdon <lephuocdon2002@gmail.com>
2025-03-08 00:21:39 +0700

    update fetchProducts & Pagination

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import vn.hoidanit.laptopshop.domain.User;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAll(Pageable page);
 
 }

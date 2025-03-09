f3e8f3c0af8b3a5f959d4dc4dc69bf0edb2b6cde
lephuocdon <lephuocdon2002@gmail.com>
2025-03-09 15:33:23 +0700

    create specification

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -147,7 +147,9 @@ public class ItemController {
     }
 
     @GetMapping("/products")
-    public String getProductPage(Model model, @RequestParam("page") Optional<String> pageOptional) {
+    public String getProductPage(Model model,
+            @RequestParam("page") Optional<String> pageOptional,
+            @RequestParam("name") Optional<String> nameOptional) {
         int page = 1;
         try {
             if (pageOptional.isPresent()) {
@@ -161,8 +163,10 @@ public class ItemController {
             // TODO: handle exception
         }
 
+        String name = nameOptional.get();
+
         Pageable pageable = PageRequest.of(page - 1, 6);
-        Page<Product> prs = this.productService.fetchProducts(pageable);
+        Page<Product> prs = this.productService.fetchProducts(pageable, name);
         List<Product> products = prs.getContent();
 
         model.addAttribute("products", products);
